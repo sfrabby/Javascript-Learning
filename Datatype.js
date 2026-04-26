df21099bf3006da9264d765005de6b1aee2209a4
sfrabby <fazlerabby6732@gmail.com>
2026-04-26 23:52:13 +0600

    Bigint

--- Datatype.js
+++ Datatype.js
@@ -4,4 +4,27 @@
 
 let firstName = 'Fazle';
 let lastName = 'Rabby';
-console.log(firstName+ lastName);
+let fullName = firstName+ lastName;
+console.log(fullName);
+console.log(typeof fullName);
+
+
+//Number 
+
+let a = 299;
+let b = 300;
+let c = a+b;
+console.log(a);
+console.log(typeof a);
+
+
+//Bigint
+
+let bigNumber = 123456789n;
+let bigNumber2 = 987654321n;
+
+let sum = bigNumber * bigNumber2;
+
+console.log(sum);
+console.log(typeof sum);
+
