64d24994ccb536de57eaa5726a9a2678f8fc2e55
sfrabby <fazlerabby6732@gmail.com>
2026-04-28 00:22:17 +0600

    Object letarecy done

--- Obj.js
+++ Obj.js
@@ -5,7 +5,38 @@ const mobileModel = {
     Camera : ['45MP', '33'],
     'Selfi Camera' : '25Mp',
     hasTelePhoto : true,
+    mm : function(){
+       return `This is mobile ${this.brand}` 
+    },
+    Battry : {
+        mah : 4000,
+    }
 }
 
-console.log(mobileModel);
+console.log(mobileModel.mm());
 console.log(mobileModel.Camera);
+
+
+Obj1 = {
+    a : 1,
+    b : 2, 
+    c : 3
+}
+
+Obj2 = {
+    x : 1,
+    y : 2, 
+    z : 3
+}
+
+Obj3 = {
+    p : 1,
+    q : 2, 
+    r : 3
+}
+
+finalObj = {...Obj1, ...Obj2, ...Obj3}
+//finalObj = Object.assign({}, Obj1, Obj2, Obj3)
+console.log(finalObj);
+console.log(mobileModel.Battry.mah);
+
