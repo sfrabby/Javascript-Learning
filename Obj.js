a9eeb839472ba7e61cfcf3a7673919954a23f220
sfrabby <fazlerabby6732@gmail.com>
2026-04-27 23:21:47 +0600

    Javascript object

---
+++ Obj.js
@@ -0,0 +1,11 @@
+const mobileModel = {
+    brand : 'symphony',
+    Model : 'Innova 30',
+    Price : 12500,
+    Camera : ['45MP', '33'],
+    'Selfi Camera' : '25Mp',
+    hasTelePhoto : true,
+}
+
+console.log(mobileModel);
+console.log(mobileModel.Camera);
--- function.js
+++ function.js
@@ -2,3 +2,16 @@ function myfunction(){
     console.log('My function called');
 }
 myfunction()
+
+let add = (a,b)=> a+b;
+
+console.log(10,20);
+
+function fun1 (firstName){
+    function fun2(){
+console.log('Hi iam'+firstName);
+
+    }
+    return fun2();
+}
+fun1('Rabby')
