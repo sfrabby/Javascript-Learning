0883227c678129d45b3903c965c7d684b71a956c
sfrabby <fazlerabby6732@gmail.com>
2026-04-29 22:36:07 +0600

    Javascript array

---
+++ Array.js
@@ -0,0 +1,10 @@
+const language = ['dart','flutter', 'Rest API', 'Getx', 'firebase']
+
+console.log(language);
+console.log(language.toString);
+console.log(language[3]);
+console.log(language[1]);
+
+language[5] = 'supabase';
+
+console.log(language);
