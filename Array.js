520fe84ad54b4ddf08dae9c027f75a54ede5e981
sfrabby <fazlerabby6732@gmail.com>
2026-04-29 22:46:16 +0600

    array all

--- Array.js
+++ Array.js
@@ -1,10 +1,34 @@
 const language = ['dart','flutter', 'Rest API', 'Getx', 'firebase']
 
 console.log(language);
-console.log(language.toString);
+console.log(language.toString());
 console.log(language[3]);
 console.log(language[1]);
+
+//Add Item
 
 language[5] = 'supabase';
 
 console.log(language);
+
+console.log(language.length);
+
+
+//First item added
+console.log(language.unshift('basic Dart'));
+console.log(language);
+
+//Delete fist Item
+
+language.shift();
+console.log(language);
+
+//Last Item delete
+
+language.pop();
+console.log(language);
+
+
+//Last Item added
+
+language.push();
