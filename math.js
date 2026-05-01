150d255b6742f222524a0b1cb0d74b96492232d1
sfrabby <fazlerabby6732@gmail.com>
2026-05-01 23:59:29 +0600

    min, max , PI, sign value

--- math.js
+++ math.js
@@ -10,3 +10,12 @@ console.log(Math.pow(2,3));
 //Root value
 
 console.log(Math.sqrt(64));
+
+//Minimum value
+console.log(Math.min(1,2,3,4,5,6,7,8));
+//Max value
+console.log(Math.max(1,2,3,4,5,6,7,8));
+//PI value
+console.log(Math.PI);
+//Sign value
+console.log(Math.sign(45*Math.PI/180));
