8fb7ad28d190e0e182c554821793ef10bd37dc94
sfrabby <fazlerabby6732@gmail.com>
2026-05-01 23:53:05 +0600

    Floor, ceail, pow, squrt

--- math.js
+++ math.js
@@ -1,2 +1,12 @@
 let num = 30.9484;
 console.log(Math.round(num));
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
+
+//Squre
+
+console.log(Math.pow(2,3));
+
+//Root value
+
+console.log(Math.sqrt(64));
